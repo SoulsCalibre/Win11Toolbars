c97f46728b86882cac6c7406befcc288e170a2b1
SoulsCalibre <p0990091@Gmail.com>
2023-05-10 23:11:35 -0700

    Update build_list_widget.bat
    Make batch file more reliable

--- build_list_widget.bat
+++ build_list_widget.bat
@@ -1,7 +1,33 @@
 @echo off
 pyinstaller list_widget.pyw
-timeout 1
+
+:wait_for_build
+IF NOT EXIST "build" (
+  timeout /t 1 >nul
+  GOTO wait_for_build
+)
+
 rd /s /q build
-move dist\caller .
+
+:wait_for_list_widget
+IF NOT EXIST "dist\list_widget" (
+  timeout /t 1 >nul
+  GOTO wait_for_list_widget
+)
+
+move dist\list_widget .
+
+:wait_for_move
+IF NOT EXIST "dist" (
+  timeout /t 1 >nul
+  GOTO wait_for_move
+)
+
+IF NOT EXIST "list_widget" (
+  timeout /t 1 >nul
+  GOTO wait_for_move
+)
+
 rd /s /q dist
-del caller.spec
+
+del list_widget.spec
